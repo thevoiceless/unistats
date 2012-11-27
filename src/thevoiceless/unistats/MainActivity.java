ffb50181be013fc7836117c25f2c5f11db62907c
Riley Moses <victi.resurgo@gmail.com>
2012-11-26 19:03:45 -0700

    Initial commit

---
+++ src/thevoiceless/unistats/MainActivity.java
@@ -0,0 +1,25 @@
+package thevoiceless.unistats;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+
+public class MainActivity extends SherlockActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
