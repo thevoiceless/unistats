ddca51e2bedeb5073af28d7742b21fc428a53060
Riley Moses <victi.resurgo@gmail.com>
2012-12-02 13:31:55 -0700

    Add launcher icon, exit app after switching to rides from achievements

--- src/thevoiceless/unistats/AchievementsActivity.java
+++ src/thevoiceless/unistats/AchievementsActivity.java
@@ -80,7 +80,7 @@ public class AchievementsActivity extends SherlockActivity
 				return true;
 			case R.id.menu_rides:
 				i = new Intent(this, RidesActivity.class);
-				i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+				i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				startActivity(i);
 				return true;
 			default:
