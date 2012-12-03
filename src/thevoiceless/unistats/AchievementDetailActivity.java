68d0b030cb44e508f60234de8291aed9cb0c4ebe
Riley Moses <victi.resurgo@gmail.com>
2012-12-02 17:35:25 -0700

    Set date for achievement

--- src/thevoiceless/unistats/AchievementDetailActivity.java
+++ src/thevoiceless/unistats/AchievementDetailActivity.java
@@ -1,12 +1,20 @@
 package thevoiceless.unistats;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.actionbarsherlock.app.SherlockActivity;
@@ -14,6 +22,11 @@ import com.actionbarsherlock.view.MenuItem;
 
 public class AchievementDetailActivity extends SherlockActivity
 {
+	private static final SimpleDateFormat MONTH_FORMAT = new SimpleDateFormat("MMMMMMMMMM");
+	private static final SimpleDateFormat DAY_FORMAT = new SimpleDateFormat("d");
+	private static final SimpleDateFormat YEAR_FORMAT = new SimpleDateFormat("yyyy");
+	private static Calendar cal = Calendar.getInstance();
+	private static String errors;
 	private EditText name, distance, pedals, month, day, year;
 	private CheckBox setDistance, setPedals, anyDate;
 	private Button setDate, save;
@@ -70,6 +83,7 @@ public class AchievementDetailActivity extends SherlockActivity
 		anyDate.setOnCheckedChangeListener(dateCheckboxChange);
 		setDistance.setOnCheckedChangeListener(distanceCheckboxChange);
 		setPedals.setOnCheckedChangeListener(pedalsCheckboxChange);
+		setDate.setOnClickListener(pressDateButton);
 	}
 	
 	private void enableDistance()
@@ -116,12 +130,43 @@ public class AchievementDetailActivity extends SherlockActivity
 	{
 		month.setEnabled(false);
 		month.setFocusable(false);
+		month.setText("");
 		day.setEnabled(false);
 		day.setFocusable(false);
+		day.setText("");
 		year.setEnabled(false);
 		year.setFocusable(false);
+		year.setText("");
 		setDate.setEnabled(false);
 		setDate.setFocusable(false);
+	}
+	
+	private void updateDisplayedDate()
+	{
+		Date chosenDate = cal.getTime();
+//		Log.v("updateDisplayedDate", chosenDate.toString());
+		month.setText(MONTH_FORMAT.format(chosenDate));
+		day.setText(DAY_FORMAT.format(chosenDate));
+		year.setText(YEAR_FORMAT.format(chosenDate));		
+	}
+	
+	private void updateCalendar()
+	{
+		StringBuilder dateString = new StringBuilder();
+		dateString.append(month.getText().toString());
+		dateString.append(" ");
+		dateString.append(day.getText().toString());
+		dateString.append(" ");
+		dateString.append(year.getText().toString());
+		
+		try
+		{
+			cal.setTime(new SimpleDateFormat("MMMMMMMMMM d yyyy").parse(dateString.toString()));
+		}
+		catch (Exception e)
+		{
+			Log.wtf("ohshit", getString(R.string.exception_setting_date));
+		}
 	}
 	
 	/* LISTENERS */
@@ -171,6 +216,34 @@ public class AchievementDetailActivity extends SherlockActivity
 			{
 				disablePedals();
 			}
+		}
+	};
+	
+	OnClickListener pressDateButton = new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			updateCalendar();
+			
+			new DatePickerDialog(AchievementDetailActivity.this, 
+					selectDate, 
+					cal.get(Calendar.YEAR), 
+					cal.get(Calendar.MONTH), 
+					cal.get(Calendar.DAY_OF_MONTH))
+			.show();			
+		}
+	};
+	
+	DatePickerDialog.OnDateSetListener selectDate = new DatePickerDialog.OnDateSetListener()
+	{
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,	int dayOfMonth)
+		{
+			cal.set(Calendar.YEAR, year);
+			cal.set(Calendar.MONTH, monthOfYear);
+			cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+			updateDisplayedDate();
 		}
 	};
 }
