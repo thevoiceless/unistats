f4e4bf4db7a28d41fd69d6cdaf29a4324e20e4c5
Riley Moses <victi.resurgo@gmail.com>
2012-12-09 13:23:07 -0700

    Share via Google+

--- src/thevoiceless/unistats/AchievementUnlockedActivity.java
+++ src/thevoiceless/unistats/AchievementUnlockedActivity.java
@@ -3,8 +3,10 @@ package thevoiceless.unistats;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.ShareCompat;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -20,6 +22,7 @@ public class AchievementUnlockedActivity extends Activity
 	private ImageButton google, facebook, twitter, tumblr;
 	private Button copyToClipboard;
 	private String achievement;
+	private String whatIDid;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -40,37 +43,102 @@ public class AchievementUnlockedActivity extends Activity
 		tumblr = (ImageButton) findViewById(R.id.buttonTumblr);
 		copyToClipboard = (Button) findViewById(R.id.buttonCopyToClipboard);
 		achievement = getIntent().getStringExtra(ACHIEVEMENT_INFO_KEY);
+		whatIDid = achievement.replaceFirst("You", "I") + " " + getString(R.string.on_a_unicycle);
 		
 		accomplishment.setText(achievement);
 	}
 	
 	private void setListeners()
 	{
+		google.setOnClickListener(shareOnGooglePlus);
+		facebook.setOnClickListener(shareOnFacebook);
+		twitter.setOnClickListener(shareOnTwitter);
+		tumblr.setOnClickListener(shareOnTumblr);
 		copyToClipboard.setOnClickListener(pressCopyToClipboard);
+	}
+	
+	@SuppressLint("NewApi")
+	private void copyToClipboard()
+	{
+		// Use android.content.ClipboardManager on Honeycomb and higher
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB)
+		{
+		     android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
+		     ClipData clip = ClipData.newPlainText("achievement", whatIDid);
+		     clipboard.setPrimaryClip(clip); 
+		}
+		// Use android.text.ClipboardManager on anything older than Honeycomb
+		else
+		{
+		    android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
+		    clipboard.setText(whatIDid);
+		}
+		Toast.makeText(AchievementUnlockedActivity.this, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
 	}
 	
 	/* LISTENERS */
 	
 	OnClickListener pressCopyToClipboard = new OnClickListener()
 	{
-		@SuppressLint("NewApi")
 		@Override
 		public void onClick(View v)
 		{
-			// Use android.content.ClipboardManager on Honeycomb and higher
-			if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB)
+			copyToClipboard();
+		}
+	};
+	
+	// Google+
+	OnClickListener shareOnGooglePlus = new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			try
 			{
-			     android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
-			     ClipData clip = ClipData.newPlainText("achievement", achievement.replaceFirst("You", "I"));
-			     clipboard.setPrimaryClip(clip); 
+				Intent shareIntent = ShareCompat.IntentBuilder.from(AchievementUnlockedActivity.this)
+				   .setText(whatIDid)
+				   .setType("text/plain")
+				   .getIntent()
+				   .setPackage("com.google.android.apps.plus");
+				
+				startActivity(shareIntent);
 			}
-			// Use android.text.ClipboardManager on anything older than Honeycomb
-			else
+			catch (Exception e)
 			{
-			    android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
-			    clipboard.setText(achievement.replaceFirst("You", "I"));
+				Intent shareIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://plus.google.com/u/0/"));
+				copyToClipboard();
+				startActivity(shareIntent);
 			}
-			Toast.makeText(getApplicationContext(), "Text copied to clipboard", Toast.LENGTH_SHORT).show();
+		}
+	};
+	
+	// Facebook
+	OnClickListener shareOnFacebook = new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			
+		}
+	};
+	
+	// Twitter
+	OnClickListener shareOnTwitter = new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			
+		}
+	};
+	
+	// Tumblr
+	OnClickListener shareOnTumblr = new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			
 		}
 	};
 }
